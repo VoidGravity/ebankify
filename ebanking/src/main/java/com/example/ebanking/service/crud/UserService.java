4072d65de4d490a9730b9223e86f42447b873c09
VoidGravity <abdellahbardichwork@gmail.com>
2024-10-31 16:53:26 +0100

    class service to handling process between controllers and repository USER

--- ebanking/src/main/java/com/example/ebanking/service/crud/UserService.java
+++ ebanking/src/main/java/com/example/ebanking/service/crud/UserService.java
@@ -1,4 +1,43 @@
 package com.example.ebanking.service.crud;
 
+import com.example.ebanking.entity.User;
+import com.example.ebanking.repository.crud.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.update(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                            .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                            .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.getAllUsers();
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.delete(id);
+    }
 }
