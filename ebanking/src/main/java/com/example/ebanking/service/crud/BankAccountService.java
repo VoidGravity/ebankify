b2c0d401f9fd481d0a6cabd927eaace328a7e77d
VoidGravity <abdellahbardichwork@gmail.com>
2024-11-01 17:13:08 +0100

    class service to handle all the operation of the controller be fo and passing hem to repository class (BanckAccountRepository.java)

--- ebanking/src/main/java/com/example/ebanking/service/crud/BankAccountService.java
+++ ebanking/src/main/java/com/example/ebanking/service/crud/BankAccountService.java
@@ -1,4 +1,42 @@
 package com.example.ebanking.service.crud;
 
+import com.example.ebanking.entity.BankAccount;
+import com.example.ebanking.repository.crud.BanckAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BankAccountService {
+    private BanckAccountRepository bankAccountRepository;
+
+    @Autowired
+    public BankAccountService(BanckAccountRepository bankAccountRepository) {
+        this.bankAccountRepository = bankAccountRepository;
+    }
+
+    public BankAccount createAccount(BankAccount bankAccount) {
+        return bankAccountRepository.createAccount(bankAccount);
+    }
+
+    public BankAccount updateAccount(BankAccount bankAccount) {
+        return bankAccountRepository.updateAccount(bankAccount);
+    }
+
+    public void deleteAccount(Long bankAccountId) {
+        bankAccountRepository.deleteAccount(bankAccountId);
+    }
+
+    public BankAccount getAccountById(Long bankAccountId) {
+        return bankAccountRepository.getAccountById(bankAccountId);
+    }
+
+    public BankAccount getAccountByAccountNumber(String accountNumber) {
+        return bankAccountRepository.getAccountByAccountNumber(accountNumber);
+    }
+
+    public List<BankAccount> getAllAccounts() {
+        return bankAccountRepository.getAllAccounts();
+    }
 }
